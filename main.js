dc101416c7e615f783badb513604c886cf2c7e90
fauzan0130 <89979695+fauzan0130@users.noreply.github.com>
2023-11-23 21:27:34 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function openFunction(){
+    document.getElementById("menu").style.width="250px";
+    // document.getElementById("main").style.marginLeft="250px";
+    // document.getElementById("main").innerHTML="Click on cross Element and Close Menu";
+}
+function closeFunction(){
+    document.getElementById("menu").style.width="0px";
+    document.getElementById("main").style.marginLeft="0px";
+    document.getElementById("main").innerHTML="&#9776;";
+}
